3ce39d407cd2b3028421b44fabd2ff62dc9121d3
Nicolas SCHOEMAEKER <nicolas.schoemaeker@qredo.com>
2023-07-19 10:35:46 +0200

    Add miden back as a dependency to nix

--- nix/project.nix
+++ nix/project.nix
@@ -1,3 +1,24 @@
+let
+  miden = nixpkgs: nixpkgs.rustPlatform.buildRustPackage rec {
+    pname = "miden";
+    version = "0.5.0";
+    src = nixpkgs.fetchFromGitHub {
+      owner = "0xPolygonMiden";
+      repo = "miden-vm";
+      rev = "v${version}";
+      sha256 = "sha256-bDILZ8vvxjB/JN6tyhTNKxjQB/35M47grxFqToesH9E=";
+    };
+    cargoLock.lockFile = ./miden_cargo.lock;
+    postPatch = ''
+      ln -s ${./miden_cargo.lock} Cargo.lock
+    '';
+    buildType = "release";
+    buildFeatures = [ "executable" "concurrent" ];
+    nativeBuildInputs = with nixpkgs; [ rustc ];
+    doCheck = false;
+  };
+in
+
 (import ./defaults.nix).projects.haskell ({nixpkgs, ...}: {
   compiler = "ghc96";
 
@@ -7,5 +28,6 @@
     nightfall = ../.;
   };
 
-  shellTools = with nixpkgs; [];
+  # shellTools = with nixpkgs; [];
+  shellTools = [ (miden nixpkgs) ];
 })
